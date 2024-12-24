d1c7d8cfcdb806861be167d7299aa2c99b1282ed
root <root@september-rain.com>
2024-12-24 15:12:54 +0900

    メール認証実装

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,49 +1,52 @@
-@extends('layouts.app')
-@section('content')
-<h2>Register</h2>
+<x-guest-layout>
+    <form method="POST" action="{{ route('register') }}">
+        @csrf
 
-@if ($errors->any())
-    <div>
+        <!-- Name -->
         <div>
-            Something went wrong!
+            <x-input-label for="name" :value="__('Name')" />
+            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+            <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>
-                    {{ $error }}
-                </li>
-            @endforeach
-        </ul>
-    </div>
-@endif
-<form action="/register" method="POST">
-    @csrf
-    <div>
-        <label for="name">Name</label>
-        <input type="text" name="name" id="name" value="{{ old('name') }}" autofocus>
-    </div>
-    <div>
-        <label for="email">Email</label>
-        <input type="text" name="email" id="email" value="{{ old('email') }}" >
-    </div>
-    <div>
-        <label for="email">Password</label>
-        <input type="password" name="password" id="password" value="{{ old('password') }}" >
-    </div>
-    <div>
-        <label for="email_confirmation">Password Confirmation</label>
-        <input type="password" name="password_confirmation" id="password_confirmation" value="{{ old('password_confirmation') }}" >
-    </div>
-    <div>
-        <button>Regist</button>
-    </div>
-</form>
-<div>
-    <label for="remember">
-        <input type="checkbox" name="remember" id="remember">
-        <span>
-            Remember me
-        </span>
-    </label>
-</div>
-@endsection
+
+        <!-- Email Address -->
+        <div class="mt-4">
+            <x-input-label for="email" :value="__('Email')" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        </div>
+
+        <!-- Password -->
+        <div class="mt-4">
+            <x-input-label for="password" :value="__('Password')" />
+
+            <x-text-input id="password" class="block mt-1 w-full"
+                            type="password"
+                            name="password"
+                            required autocomplete="new-password" />
+
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        </div>
+
+        <!-- Confirm Password -->
+        <div class="mt-4">
+            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+
+            <x-text-input id="password_confirmation" class="block mt-1 w-full"
+                            type="password"
+                            name="password_confirmation" required autocomplete="new-password" />
+
+            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+        </div>
+
+        <div class="flex items-center justify-end mt-4">
+            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
+                {{ __('Already registered?') }}
+            </a>
+
+            <x-primary-button class="ms-4">
+                {{ __('Register') }}
+            </x-primary-button>
+        </div>
+    </form>
+</x-guest-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,20 @@
 <?php
-use App\Http\Controllers\Auth\AuthController;
-use App\Http\Controllers\Auth\RegisterController;
-use App\Http\Controllers\Auth\LogoutController;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\NoteController;
-use App\Http\Controllers\PateController;
-use App\Http\Controllers\PateCommentController;
-use App\Http\Controllers\Auth\ForgotPasswordLinkController;
+
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-Route::get('/', [HomeController::class, 'index']);
-Route::get('/register', [RegisterController::class, 'create']);
-Route::post('/register', [RegisterController::class, 'store']);
-Route::get('/login', [AuthController::class, 'create']);
-Route::post('/login', [AuthController::class, 'store']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth');
-Route::post('/forgot-password', [ForgotPasswordLinkController::class, 'store']);
-Route::post('/forgot-password/{token}', [ForgotPasswordLinkController::class, 'reset']);
-Route::group(['prefix' => 'notes'], function () {
-    Route::get('/', [NoteController::class, 'index']);
-    Route::get('/post',  [NoteController::class, 'create']);
-    Route::post('/post',  [NoteController::class, 'store']);
-    Route::get('/delete/{id}',  [NoteController::class, 'destroy']);
-    Route::post('/update/{id}',  [NoteController::class, 'update']);
+Route::get('/', function () {
+    return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
