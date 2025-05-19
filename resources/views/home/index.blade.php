797b29b05fcd180e0ab765a282e9fd4a1deb070a
Kazumasa Sato <seisaku+sato_kazumasa@kurapital.com>
2025-05-19 18:21:50 +0900

    レイアウト修正（３）

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,25 +3,25 @@
             <main style="padding: 56px 0px 36px;">
                 <h2>Home</h2>
                 <div class="row">
-                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4" style="height: 200px;	background-color: whitesmoke; padding: 2px; margin: 2px;display: flex;">
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4" style="height: 200px; background-color: whitesmoke; padding: 2px; margin: 2px;">
                         NEWS
                     </div>
-                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4" style="height: 200px;	background-color: whitesmoke; padding: 2px; margin: 2px;">
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4" style="height: 200px; background-color: whitesmoke; padding: 2px; margin: 2px;">
                         NEWS
                     </div>
-                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4" style="height: 200px;	background-color: whitesmoke; padding: 2px; margin: 2px;">
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4" style="height: 200px; background-color: whitesmoke; padding: 2px; margin: 2px;">
                         NEWS
                     </div>
-                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4" style="height: 200px;	background-color: whitesmoke; padding: 2px; margin: 2px;">
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4" style="height: 200px; background-color: whitesmoke; padding: 2px; margin: 2px;">
                         NEWS
                     </div>
-                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4" style="height: 200px;	background-color: whitesmoke; padding: 2px; margin: 2px;">
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4" style="height: 200px; background-color: whitesmoke; padding: 2px; margin: 2px;">
                         NEWS
                     </div>
-                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4" style="height: 200px;	background-color: whitesmoke; padding: 2px; margin: 2px;">
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4" style="height: 200px; background-color: whitesmoke; padding: 2px; margin: 2px;">
                         NEWS
                     </div>
-                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4" style="height: 200px;	background-color: whitesmoke; padding: 2px; margin: 2px;">
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4" style="height: 200px; background-color: whitesmoke; padding: 2px; margin: 2px;">
                         NEWS
                     </div>
                 </div>
