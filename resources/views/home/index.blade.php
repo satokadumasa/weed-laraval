976077de564e5a1793f58c08d5e318cdcebb1c33
Kazumasa Sato <seisaku+sato_kazumasa@kurapital.com>
2025-05-19 18:49:56 +0900

    レイアウト修正（４）

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,26 +3,40 @@
             <main style="padding: 56px 0px 36px;">
                 <h2>Home</h2>
                 <div class="row">
-                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4" style="height: 200px; background-color: whitesmoke; padding: 2px; margin: 2px;">
-                        NEWS
+                    <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12" style="height: 200px; background-color: whitesmoke; padding: 1px;">
+                        <div style="height: 196px; margin: 1px; background-color: white;">
+                            NEWS
+                        </div>
                     </div>
-                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4" style="height: 200px; background-color: whitesmoke; padding: 2px; margin: 2px;">
-                        NEWS
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4" style="height: 200px; background-color: whitesmoke; padding: 1px;">
+                        <div style="height: 196px; margin: 1px; background-color: white;">
+                            NEWS
+                        </div>
                     </div>
-                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4" style="height: 200px; background-color: whitesmoke; padding: 2px; margin: 2px;">
-                        NEWS
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4" style="height: 200px; background-color: whitesmoke; padding: 1px;">
+                        <div style="height: 196px; margin: 1px; background-color: white;">
+                            NEWS
+                        </div>
                     </div>
-                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4" style="height: 200px; background-color: whitesmoke; padding: 2px; margin: 2px;">
-                        NEWS
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4" style="height: 200px; background-color: whitesmoke; padding: 1px;">
+                        <div style="height: 196px; margin: 1px; background-color: white;">
+                            NEWS
+                        </div>
                     </div>
-                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4" style="height: 200px; background-color: whitesmoke; padding: 2px; margin: 2px;">
-                        NEWS
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4" style="height: 200px; background-color: whitesmoke; padding: 1px;">
+                        <div style="height: 196px; margin: 1px; background-color: white;">
+                            NEWS
+                        </div>
                     </div>
-                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4" style="height: 200px; background-color: whitesmoke; padding: 2px; margin: 2px;">
-                        NEWS
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4" style="height: 200px; background-color: whitesmoke; padding: 1px;">
+                        <div style="height: 196px; margin: 1px; background-color: white;">
+                            NEWS
+                        </div>
                     </div>
-                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4" style="height: 200px; background-color: whitesmoke; padding: 2px; margin: 2px;">
-                        NEWS
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4" style="height: 200px; background-color: whitesmoke; padding: 1px;">
+                        <div style="height: 196px; margin: 1px; background-color: white;">
+                            NEWS
+                        </div>
                     </div>
                 </div>
             </main>
