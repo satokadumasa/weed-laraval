755703e0792975125ff3959a6773be1cdf4af979
Kazumasa Sato <seisaku+sato_kazumasa@kurapital.com>
2025-05-20 11:18:17 +0900

    レイアウト修正（５）

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -4,38 +4,49 @@
                 <h2>Home</h2>
                 <div class="row">
                     <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12" style="height: 200px; background-color: whitesmoke; padding: 1px;">
-                        <div style="height: 196px; margin: 1px; background-color: white;">
-                            NEWS
+                        <div style="height: 196px; margin: 1px; padding: 5px; background-color: white;">
+                            0001
                         </div>
                     </div>
-                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4" style="height: 200px; background-color: whitesmoke; padding: 1px;">
-                        <div style="height: 196px; margin: 1px; background-color: white;">
-                            NEWS
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4" style="height: 300px; background-color: whitesmoke; padding: 1px;">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12" style="height: 150px; background-color: whitesmoke; padding: 1px;">
+                                    <div style="height: 146px; margin: 1px; padding: 5px; background-color: white;">
+                                        0002-01
+                                    </div>
+                                </div>
+                                <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12" style="height: 150px; background-color: whitesmoke; padding: 1px;">
+                                    <div style="height: 146px; margin: 1px; padding: 5px; background-color: white;">
+                                        0002-02
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4" style="height: 200px; background-color: whitesmoke; padding: 1px;">
-                        <div style="height: 196px; margin: 1px; background-color: white;">
-                            NEWS
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4" style="height: 300px; background-color: whitesmoke; padding: 1px;">
+                        <div style="height: 296px; margin: 1px; padding: 5px; background-color: white;">
+                            0003
                         </div>
                     </div>
-                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4" style="height: 200px; background-color: whitesmoke; padding: 1px;">
-                        <div style="height: 196px; margin: 1px; background-color: white;">
-                            NEWS
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4" style="height: 300px; background-color: whitesmoke; padding: 1px;">
+                        <div style="height: 296px; margin: 1px; padding: 5px; background-color: white;">
+                            0004
                         </div>
                     </div>
-                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4" style="height: 200px; background-color: whitesmoke; padding: 1px;">
-                        <div style="height: 196px; margin: 1px; background-color: white;">
-                            NEWS
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4" style="height: 300px; background-color: whitesmoke; padding: 1px;">
+                        <div style="height: 296px; margin: 1px; padding: 5px; background-color: white;">
+                            0005
                         </div>
                     </div>
-                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4" style="height: 200px; background-color: whitesmoke; padding: 1px;">
-                        <div style="height: 196px; margin: 1px; background-color: white;">
-                            NEWS
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4" style="height: 300px; background-color: whitesmoke; padding: 1px;">
+                        <div style="height: 296px; margin: 1px; padding: 5px; background-color: white;">
+                            0006
                         </div>
                     </div>
-                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4" style="height: 200px; background-color: whitesmoke; padding: 1px;">
-                        <div style="height: 196px; margin: 1px; background-color: white;">
-                            NEWS
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4" style="height: 300px; background-color: whitesmoke; padding: 1px;">
+                        <div style="height: 296px; margin: 1px; padding: 5px; background-color: white;">
+                            0007
                         </div>
                     </div>
                 </div>
