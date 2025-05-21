19b0b74e1ae9c3eaabd0a69ae0022c47ca0562a1
Kazumasa Sato <seisaku+sato_kazumasa@kurapital.com>
2025-05-21 18:17:29 +0900

    画面サイズによる表示切替

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -29,10 +29,53 @@
 </x-app-layout> --}}
 @extends('layouts.app')
 @section('content')
-            <h2>Profile</h2>
-            <div class="row align-items-start">
-                <div class="col">
-
+            <main style="padding: 56px 0px 36px;">
+                <h2>Home</h2>
+                <div class="row">
+                    <div class="col-12 col-sm-12 col-md-3 col-lg-2 col-xl-2 col-xxl-2" style="height: 200px; background-color: whitesmoke; padding: 1px;">
+                        <div style="height: 196px; margin: 1px; padding: 5px; background-color: white;">
+                            [写真]
+                        </div>
+                    </div>
+                    <div class="col-12 col-sm-12 col-md-9 col-lg-10 col-xl-10 col-xxl-10" style="height: 200px; background-color: whitesmoke; padding: 1px;">
+                        <div style="height: 196px; margin: 1px; padding: 5px; background-color: white;">
+                            [ハンドルネーム]<br>
+                            [ハンドルネーム]<br>
+                            [ハンドルネーム]<br>
+                            [ハンドルネーム]<br>
+                            [ハンドルネーム]<br>
+                        </div>
+                    </div>
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6" style="height: 300px; background-color: whitesmoke; padding: 1px;">
+                        <div style="height: 296px; margin: 1px; padding: 5px; background-color: white;">
+                            0003
+                        </div>
+                    </div>
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6" style="height: 300px; background-color: whitesmoke; padding: 1px;">
+                        <div style="height: 296px; margin: 1px; padding: 5px; background-color: white;">
+                            0002
+                        </div>
+                    </div>
+                    <div class="d-none d-sm-block col-lg-4 col-xl-4 col-xxl-4" style="height: 300px; background-color: whitesmoke; padding: 1px;">
+                        <div style="height: 296px; margin: 1px; padding: 5px; background-color: white;">
+                            0004
+                        </div>
+                    </div>
+                    <div class="d-none d-sm-block col-lg-4 col-xl-4 col-xxl-4" style="height: 300px; background-color: whitesmoke; padding: 1px;">
+                        <div style="height: 296px; margin: 1px; padding: 5px; background-color: white;">
+                            0005
+                        </div>
+                    </div>
+                    <div class="d-none d-sm-block col-lg-4 col-xl-4 col-xxl-4" style="height: 300px; background-color: whitesmoke; padding: 1px;">
+                        <div style="height: 296px; margin: 1px; padding: 5px; background-color: white;">
+                            0006
+                        </div>
+                    </div>
+                    <div class="d-none d-sm-block col-lg-4 col-xl-4 col-xxl-4" style="height: 300px; background-color: whitesmoke; padding: 1px;">
+                        <div style="height: 296px; margin: 1px; padding: 5px; background-color: white;">
+                            0007
+                        </div>
+                    </div>
                 </div>
-            </div>
+            </main>
 @endsection
