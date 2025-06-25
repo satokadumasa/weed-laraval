641a7949e9a4516441295d0f997e0fe757426760
Kazumasa Sato <seisaku+sato_kazumasa@kurapital.com>
2025-06-25 11:28:29 +0900

    QRコード読込機能(2)

--- resources/views/ticket/qr_read.blade.php
+++ resources/views/ticket/qr_read.blade.php
@@ -9,7 +9,47 @@
         <canvas id="canvas"></canvas>
     </div>
     <script>
+        const userMedia = {video: {facingMode: "environment"}};
+        navigator.mediaDevices.getUserMedia(userMedia).then((stream)=>{
+            video.srcObject = stream;
+            video.setAttribute("playsinline", true);
+            video.play();
+            startTick();
+        });
 
+        function startTick(){
+            msg.innerText = "Loading video...";
+            if(video.readyState === video.HAVE_ENOUGH_DATA){
+                canvas.height = video.videoHeight;
+                canvas.width = video.videoWidth;
+                ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+                // このタイミングでQRコードを判定します
+            }
+            setTimeout(startTick, 250);
+        }
+        $(function() {
+            startTick();
+        });
     </script>
+    <style>
+        h1{
+            text-align: center;
+        }
 
+        #wrapper{
+            margin: 0px auto 0px auto;
+            width: 320px; height: auto;
+        }
+
+        #msg{
+            margin: 0px; padding: 10px;
+            background-color: lightgray;
+            text-align: center;
+        }
+
+        #canvas{
+            width: 100%; height: auto;
+            background-color: silver;
+        }        
+    </style>
 </div>@endsection
