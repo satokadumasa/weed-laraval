50a86fdd91f5058e6d1596e4f46d0ce467bbcdef
Kazumasa Sato <seisaku+sato_kazumasa@kurapital.com>
2025-06-25 11:24:48 +0900

    QRコード読込機能

--- resources/views/ticket/qr_read.blade.php
+++ resources/views/ticket/qr_read.blade.php
@@ -3,26 +3,13 @@
 <script src="https://cdn.jsdelivr.net/npm/jsqr@1.4.0/dist/jsQR.min.js">
 <script src="/js/qrread.js">
 <div>
-    <div id="loading">ブラウザのカメラの使用を許可してください。</div>
-    @if (session()->has('message'))
-    <div class="alert alert-success d-flex align-items-center" role="alert">
-        <i class="fa-solid fa-circle-check mr-1"></i>
-        {{ session('message') }}
+    <h1>jsQR</h1>
+    <div id="wrapper">
+        <div id="msg">Unable to access video stream.</div>
+        <canvas id="canvas"></canvas>
     </div>
-    @endif
-    <canvas id="canvas" hidden></canvas>
-
-    <style>
-        #canvas {
-            width: 100%;
-            height: 720px;
-        }
-    </style>
-    {{-- @pushで囲んだ内容を@stackで出力する --}}
-    @push('scripts')
     <script>
 
     </script>
-    @endpush
 
 </div>@endsection
